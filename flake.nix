7df25dde669d16175e18aa8222746db47333d442
saylesss88 <saylesss87@proton.me>
2026-08-25 16:09:32 -0400

    feat(NixOS): add a package output for people to install latest from source

--- flake.nix
+++ flake.nix
@@ -13,8 +13,25 @@
       system:
       let
         pkgs = nixpkgs.legacyPackages."${system}";
+        manifest = (pkgs.lib.importTOML ./Cargo.toml).package;
       in
       {
+        packages.default = pkgs.rustPlatform.buildRustPackage {
+          pname = manifest.name;
+          version = manifest.version;
+          src = ./.;
+          cargoLock.lockFile = ./Cargo.lock;
+
+          meta = with pkgs.lib; {
+            description = manifest.description;
+            homepage = manifest.repository;
+            license = licenses.mit;
+            mainProgram = "mdbook-rss-feed";
+          };
+        };
+        apps.default = flake-utils.lib.mkApp {
+          drv = self.packages.${system}.default;
+        };
         devShells.default = pkgs.mkShell {
           packages = [
             pkgs.rustup.out
